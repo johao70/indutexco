5cd26dcf5325f199d664d969abe367f953c218bf
johao70 <perlazajohao@gmail.com>
2019-07-24 11:46:29 -0500

    ok

--- avancesJohao/src/app/app-routing.module.ts
+++ avancesJohao/src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  // { path: '', redirectTo: '/', pathMatch: 'full' }
+  // { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
