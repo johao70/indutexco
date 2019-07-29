f012d435b0b2f4292eaeded0772d41b0e7d9ae53
johao70 <perlazajohao@gmail.com>
2019-07-28 20:32:49 -0500

    tables

--- Front_end/src/app/body/ordentrabajo/ordentrabajo.component.ts
+++ Front_end/src/app/body/ordentrabajo/ordentrabajo.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-ordentrabajo',
   templateUrl: './ordentrabajo.component.html',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdentrabajoComponent implements OnInit {
 
-  constructor() { }
+  respuesta: any[]
+  table_header: any
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getData()
+    this.table_header = [
+      {
+        id: 'id',
+        tela: 'Tela',
+        boton: 'Boton',
+        hilo: 'Hilo',
+        etiqueta: 'Etiqueta',
+        empleado: 'Empleado'
+      }
+    ]
+  }
+
+  getData = () => {
+    let tabla = 'productos'
+    this.http.get<any>(environment.API_URL + `?tabla=${tabla}`)
+        .subscribe(data => {
+            this.respuesta = data.datos
+        })
   }
 
 }
--- Front_end/src/app/body/personal/personal.component.ts
+++ Front_end/src/app/body/personal/personal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.component.html',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalComponent implements OnInit {
 
-  constructor() { }
+  respuesta: any[]
+  table_header: any
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getData()
+    this.table_header = [
+      {
+        id: 'id',
+        nombre: 'Nombre',
+        apellido: 'Apellido',
+        direccion: 'Dirección',
+        telefono: 'Teléfono',
+        idDepartamento: 'idDepartamento',
+        idCargo: 'idCargo',
+        idEstado: 'idEstado'
+      }
+    ]
+  }
+
+  getData = () => {
+    let tabla = 'empleados'
+    this.http.get<any>(environment.API_URL + `?tabla=${tabla}`)
+        .subscribe(data => {
+            this.respuesta = data.datos
+        })
   }
 
 }
