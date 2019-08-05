551c985f0a47f00903e2e9e295a54f443f3a9534
PaoNar <pen.castro@yavirac.edu.ec>
2019-08-04 15:28:14 -0900

    ok

--- Back_end/instalacion/configuracion/entorno.js
+++ Back_end/instalacion/configuracion/entorno.js
@@ -5,7 +5,7 @@ let initVar = () => {
     process.env.CONNECTION_DB = process.env.CONNECTION_DB || {
                                                                 host: '127.0.0.1', 
                                                                 user: 'postgres', 
-                                                                password: '1999', 
+                                                                password: '12345', 
                                                                 database: 'indutexco'
                                                               }
   }
